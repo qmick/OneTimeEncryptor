5537eeb504441e31dfd1f5f4522e1ee11a298ceb
CSJY <qmick@live.cn>
2018-05-23 12:30:45 +0800

    Update translation

--- mainwindow_zh.ts
+++ mainwindow_zh.ts
@@ -4,7 +4,7 @@
 <context>
     <name>CryptThread</name>
     <message>
-        <location filename="../crypt_thread.cpp" line="63"/>
+        <location filename="crypt_thread.cpp" line="63"/>
         <source>File not exists or size 0</source>
         <translation>文件不存在或者大小为0</translation>
     </message>
@@ -12,22 +12,22 @@
 <context>
     <name>Dialog</name>
     <message>
-        <location filename="../dialog.ui" line="14"/>
+        <location filename="dialog.ui" line="14"/>
         <source>Dialog</source>
         <translation>对话框</translation>
     </message>
     <message>
-        <location filename="../dialog.ui" line="36"/>
+        <location filename="dialog.ui" line="36"/>
         <source>&amp;Encrypt</source>
         <translation>加密(&amp;E)</translation>
     </message>
     <message>
-        <location filename="../dialog.ui" line="49"/>
+        <location filename="dialog.ui" line="49"/>
         <source>&amp;Cancel</source>
         <translation>退出(&amp;C)</translation>
     </message>
     <message>
-        <location filename="../dialog.ui" line="62"/>
+        <location filename="dialog.ui" line="62"/>
         <source>&amp;Decrypt</source>
         <translation>解密(&amp;D)</translation>
     </message>
@@ -35,7 +35,7 @@
 <context>
     <name>EncryptionDialog</name>
     <message>
-        <location filename="../encryption_dialog.cpp" line="8"/>
+        <location filename="encryption_dialog.cpp" line="8"/>
         <source>Message</source>
         <translation>消息</translation>
     </message>
@@ -43,287 +43,321 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="14"/>
+        <location filename="mainwindow.ui" line="14"/>
         <source>MainWindow</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="24"/>
+        <location filename="mainwindow.ui" line="24"/>
         <source>00:00:00</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="49"/>
+        <location filename="mainwindow.ui" line="49"/>
         <source>Stop</source>
         <translation>停止</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="69"/>
+        <location filename="mainwindow.ui" line="69"/>
         <source>Public key digest</source>
         <translation>公钥摘要</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="76"/>
+        <location filename="mainwindow.ui" line="76"/>
         <source>Only available in encryption</source>
         <translation>只在加密时可用</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="83"/>
+        <location filename="mainwindow.ui" line="83"/>
         <source>Auto Delete</source>
         <translation>自动删除原文件</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="103"/>
+        <location filename="mainwindow.ui" line="103"/>
         <source>&amp;File</source>
         <translation>文件(&amp;F)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="112"/>
+        <location filename="mainwindow.ui" line="116"/>
         <source>&amp;About</source>
         <translation>关于(&amp;A)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="117"/>
+        <location filename="mainwindow.ui" line="121"/>
         <source>&amp;Key</source>
         <translation>密钥(&amp;K)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="121"/>
+        <location filename="mainwindow.ui" line="125"/>
         <source>&amp;Generate key</source>
         <translation>生成密钥(&amp;G)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="133"/>
+        <location filename="mainwindow.ui" line="137"/>
         <source>&amp;Message</source>
         <translation>消息(&amp;M)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="140"/>
+        <location filename="mainwindow.ui" line="144"/>
         <source>&amp;User</source>
         <translation>用户(&amp;U)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="163"/>
-        <location filename="../mainwindow.ui" line="203"/>
+        <location filename="mainwindow.ui" line="167"/>
+        <location filename="mainwindow.ui" line="207"/>
         <source>&amp;Encrypt</source>
         <translation>加密(&amp;E)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="168"/>
+        <location filename="mainwindow.ui" line="172"/>
         <source>E&amp;xit</source>
         <translation>退出(&amp;x)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="173"/>
-        <location filename="../mainwindow.ui" line="208"/>
+        <location filename="mainwindow.ui" line="177"/>
+        <location filename="mainwindow.ui" line="212"/>
         <source>&amp;Decrypt</source>
         <translation>解密(&amp;D)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="178"/>
+        <location filename="mainwindow.ui" line="182"/>
         <source>Load p&amp;rivate key</source>
         <translation>加载私钥(&amp;r)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="183"/>
+        <location filename="mainwindow.ui" line="187"/>
         <source>Load p&amp;ublic key</source>
         <translation>加载公钥(&amp;u)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="188"/>
+        <location filename="mainwindow.ui" line="192"/>
         <source>&amp;Reset password</source>
         <translation>重置密码(&amp;R)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="193"/>
+        <location filename="mainwindow.ui" line="197"/>
         <source>ECC</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="198"/>
+        <location filename="mainwindow.ui" line="202"/>
         <source>RSA</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="213"/>
+        <location filename="mainwindow.ui" line="217"/>
         <source>&amp;Add</source>
         <translation>添加(&amp;A)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="218"/>
+        <location filename="mainwindow.ui" line="222"/>
         <source>&amp;Switch</source>
         <translation>切换(&amp;S)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="223"/>
+        <location filename="mainwindow.ui" line="227"/>
         <source>&amp;Delete</source>
         <translation>删除(&amp;D)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="56"/>
+        <location filename="mainwindow.ui" line="232"/>
+        <source>&amp;Register</source>
+        <translation>注册右键菜单(&amp;R)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="240"/>
+        <source>&amp;Unregister</source>
+        <translation>反注册右键菜单(&amp;U)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="83"/>
         <source>OneTimeEncryptor</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="156"/>
+        <location filename="mainwindow.cpp" line="190"/>
         <source>Pending</source>
         <translation>待处理</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="205"/>
-        <location filename="../mainwindow.cpp" line="269"/>
-        <location filename="../mainwindow.cpp" line="348"/>
-        <location filename="../mainwindow.cpp" line="480"/>
+        <location filename="mainwindow.cpp" line="239"/>
+        <location filename="mainwindow.cpp" line="303"/>
+        <location filename="mainwindow.cpp" line="382"/>
+        <location filename="mainwindow.cpp" line="514"/>
         <source>Input password</source>
         <translation>输入密码</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="206"/>
-        <location filename="../mainwindow.cpp" line="270"/>
-        <location filename="../mainwindow.cpp" line="291"/>
-        <location filename="../mainwindow.cpp" line="349"/>
-        <location filename="../mainwindow.cpp" line="481"/>
+        <location filename="mainwindow.cpp" line="240"/>
+        <location filename="mainwindow.cpp" line="304"/>
+        <location filename="mainwindow.cpp" line="325"/>
+        <location filename="mainwindow.cpp" line="383"/>
+        <location filename="mainwindow.cpp" line="515"/>
         <source>Password:</source>
         <translation>密码：</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="235"/>
-        <location filename="../mainwindow.cpp" line="249"/>
-        <location filename="../mainwindow.cpp" line="261"/>
-        <location filename="../mainwindow.cpp" line="326"/>
-        <location filename="../mainwindow.cpp" line="360"/>
-        <location filename="../mainwindow.cpp" line="403"/>
-        <location filename="../mainwindow.cpp" line="449"/>
-        <location filename="../mainwindow.cpp" line="473"/>
-        <location filename="../mainwindow.cpp" line="493"/>
-        <location filename="../mainwindow.cpp" line="519"/>
-        <location filename="../mainwindow.cpp" line="540"/>
-        <location filename="../mainwindow.cpp" line="580"/>
-        <location filename="../mainwindow.cpp" line="598"/>
+        <location filename="mainwindow.cpp" line="269"/>
+        <location filename="mainwindow.cpp" line="283"/>
+        <location filename="mainwindow.cpp" line="295"/>
+        <location filename="mainwindow.cpp" line="360"/>
+        <location filename="mainwindow.cpp" line="394"/>
+        <location filename="mainwindow.cpp" line="437"/>
+        <location filename="mainwindow.cpp" line="483"/>
+        <location filename="mainwindow.cpp" line="507"/>
+        <location filename="mainwindow.cpp" line="527"/>
+        <location filename="mainwindow.cpp" line="553"/>
+        <location filename="mainwindow.cpp" line="574"/>
+        <location filename="mainwindow.cpp" line="598"/>
+        <location filename="mainwindow.cpp" line="618"/>
+        <location filename="mainwindow.cpp" line="652"/>
+        <location filename="mainwindow.cpp" line="670"/>
         <source>Error</source>
         <translation>错误</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="236"/>
+        <location filename="mainwindow.cpp" line="270"/>
         <source>Cannot generate key: </source>
         <translation>无法生成密钥： </translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="249"/>
-        <location filename="../mainwindow.cpp" line="261"/>
-        <location filename="../mainwindow.cpp" line="326"/>
+        <location filename="mainwindow.cpp" line="283"/>
+        <location filename="mainwindow.cpp" line="295"/>
+        <location filename="mainwindow.cpp" line="360"/>
         <source>No user available</source>
         <translation>没有可用的用户</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="284"/>
+        <location filename="mainwindow.cpp" line="318"/>
         <source>Please input:</source>
         <translation>请输入：</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="288"/>
+        <location filename="mainwindow.cpp" line="322"/>
         <source>Username:</source>
         <translation>用户名：</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="294"/>
+        <location filename="mainwindow.cpp" line="328"/>
         <source>Key type:</source>
         <translation>密钥类型：</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="336"/>
+        <location filename="mainwindow.cpp" line="370"/>
         <source>Information</source>
         <translation>信息</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="337"/>
+        <location filename="mainwindow.cpp" line="371"/>
         <source>Successfully switched to </source>
         <translation>成功切换到 </translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="360"/>
+        <location filename="mainwindow.cpp" line="394"/>
         <source>Uable to delete user: </source>
         <translation>无法删除用户： </translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="393"/>
+        <location filename="mainwindow.cpp" line="427"/>
         <source>RSA public key loaded</source>
         <translation>已加载RSA公钥</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="398"/>
+        <location filename="mainwindow.cpp" line="432"/>
         <source>EC public key loaded</source>
         <translation>已加载ECC私钥</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="403"/>
-        <location filename="../mainwindow.cpp" line="449"/>
+        <location filename="mainwindow.cpp" line="437"/>
+        <location filename="mainwindow.cpp" line="483"/>
         <source>Unsupported Key type</source>
         <translation>不支持的密钥类型</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="413"/>
-        <location filename="../mainwindow.cpp" line="460"/>
+        <location filename="mainwindow.cpp" line="447"/>
+        <location filename="mainwindow.cpp" line="494"/>
         <source>Warning</source>
         <translation>警告</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="414"/>
+        <location filename="mainwindow.cpp" line="448"/>
         <source>Cannot load public key: </source>
         <translation>无法加载公钥： </translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="439"/>
+        <location filename="mainwindow.cpp" line="473"/>
         <source>RSA private key loaded</source>
         <translation>已加载RSA私钥</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="444"/>
+        <location filename="mainwindow.cpp" line="478"/>
         <source>EC private key loaded</source>
         <translation>已加载ECC私钥</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="461"/>
+        <location filename="mainwindow.cpp" line="495"/>
         <source>cannot open private pem file: </source>
         <translation>无法打开私钥文件： </translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="474"/>
+        <location filename="mainwindow.cpp" line="508"/>
         <source>Cannot reset password: private key not loaded</source>
         <translation>无法重置密码：私钥未加载</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="494"/>
+        <location filename="mainwindow.cpp" line="528"/>
         <source>Cannot reset password: </source>
         <translation>无法重置密码： </translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="520"/>
+        <location filename="mainwindow.cpp" line="554"/>
         <source>Public key not loaded, cannot use encryptor</source>
         <translation>公钥未加载，无法使用加密功能</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="541"/>
+        <location filename="mainwindow.cpp" line="575"/>
         <source>Private key not loaded, cannot use decryptor</source>
         <translation>私钥未加载，无法使用解密功能</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="609"/>
+        <location filename="mainwindow.cpp" line="598"/>
+        <source>Not running as admin, can&apos;t register</source>
+        <translation>注册右键菜单需要管理员权限</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="602"/>
+        <location filename="mainwindow.cpp" line="622"/>
+        <source>EncryptFile</source>
+        <translation>加密文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="603"/>
+        <location filename="mainwindow.cpp" line="623"/>
+        <source>DecryptFile</source>
+        <translation>解密文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="618"/>
+        <source>Not running as admin, can&apos;t unregister</source>
+        <translation>反注册右键菜单需要管理员权限</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="681"/>
         <source>Processing</source>
         <translation>处理中</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="618"/>
+        <location filename="mainwindow.cpp" line="690"/>
         <source>Failed</source>
         <translation>失败</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="630"/>
+        <location filename="mainwindow.cpp" line="702"/>
         <source>Stopped</source>
         <translation>已停止</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="644"/>
+        <location filename="mainwindow.cpp" line="716"/>
         <source>Finished</source>
         <translation>已完成</translation>
     </message>
@@ -331,27 +365,27 @@
 <context>
     <name>ProgressTableModel</name>
     <message>
-        <location filename="../progress_tablemodel.cpp" line="5"/>
+        <location filename="progress_tablemodel.cpp" line="5"/>
         <source>Filename</source>
         <translation>文件名</translation>
     </message>
     <message>
-        <location filename="../progress_tablemodel.cpp" line="5"/>
+        <location filename="progress_tablemodel.cpp" line="5"/>
         <source>Status</source>
         <translation>状态</translation>
     </message>
     <message>
-        <location filename="../progress_tablemodel.cpp" line="5"/>
+        <location filename="progress_tablemodel.cpp" line="5"/>
         <source>Size</source>
         <translation>大小</translation>
     </message>
     <message>
-        <location filename="../progress_tablemodel.cpp" line="5"/>
+        <location filename="progress_tablemodel.cpp" line="5"/>
         <source>Progress</source>
         <translation>进度</translation>
     </message>
     <message>
-        <location filename="../progress_tablemodel.cpp" line="5"/>
+        <location filename="progress_tablemodel.cpp" line="5"/>
         <source>Detail</source>
         <translation>详情</translation>
     </message>
